fdcaa34bc12298b22fa0752d3ebbdd1968d2bb28
Yannick <91213045+YannickZahinda@users.noreply.github.com>
2022-10-05 10:23:10 +0200

    fetch initial data

--- event-app/src/FakeEventsData.js
+++ event-app/src/FakeEventsData.js
@@ -3,15 +3,18 @@ export const EventsData = [
       id: 1,
       eventname: "University Party",
       eventlocation: "Amsterdam",
+      eventImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgpqRO9mIjFkkLPF6wHr2ZhEd8eptDhaDk_Q&usqp=CAU"
     },
     {
       id: 2,
       eventname: "Halloween",
       eventlocation: "southafrica",
+      eventImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7GCKZiNBa9ClNyyh1mB_htNzrDSUw0S-OoQ&usqp=CAU",
     },
     {
       id: 3,
       eventname: "Movie Night",
       eventlocation: "Washingtown dulles",
+      eventImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRD-6rTydlzgJ3sISiYKgWbzXxl_rK5351BKQ&usqp=CAU",
     }
   ];
